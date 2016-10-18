797a0b209d8dc9d47cf07e65e0342a3277a2d033
Charles Tai <cwtai86@gmail.com>
2016-10-17 17:54:28 -0700

    1.stateful navigation 2.separate models for navigation and recipes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,110 @@
 
-// Controller/Presentation Logic
-var init = function () {
-    $.getJSON('/api/recipes/two_person/2016_03_21', function (response) {
-      var recipes = response['two_person_plan'].recipes;
-      var template = Handlebars.compile($('#recipes-template').html());
-      Handlebars.registerPartial('recipe', $('#recipe-template').html());
-      Handlebars.registerHelper('each', function(recipes, options) {
-        var ret = "";
-        for (var i=0; i < recipes.length; i++) {
-          var recipe = recipes[i].recipe;
-          ret = ret + options.fn(recipe);
-        }
-        return ret;
+// Model
+var Model = function () {
+  var state = {};
+  var setState = (newState) => {
+    var mergedState = _.extend(state, newState);
+    // Notify all views that it has changed
+    // trigger re-render
+    console.log('mergedState', mergedState);
+    render(mergedState);
+  };
+  var getState = () => {
+    return state;
+  }
+  return {
+    getState: getState,
+    setState: setState
+  }
+}
+
+var recipesModel = new Model();
+var pickerModel = new Model();
+
+var changePlan = function (event, plan) {
+    event.preventDefault();
+    // change state
+    getPlan(plan, pickerModel.getState().date)
+}
+
+var changeDate = function (event, selected) {
+    event.preventDefault();
+    var date = selected.value;
+    // change date
+    getPlan(pickerModel.getState().plan, date)
+}
+
+var constants = {
+  "2-Person": "Two Person",
+  "Family": "Family"
+}
+
+var render = function (state) {
+    if (!state.recipes || state.recipes.length < 1) {
+      return null;
+    }
+    var recipes = state.recipes;
+    console.log('recipes', recipes);
+    var plan_type = constants[state.plan_type];
+    var date = getDate(state.date);
+    var recipesTemplate = Handlebars.compile($('#recipes-template').html());
+    var navigationTemplate = Handlebars.compile($('#navigation-template').html());
+    // Controller/View
+    // Handles rendering (compiling state and event handlers)
+    // event handlers
+    Handlebars.registerPartial('recipe', $('#recipe-template').html());
+    Handlebars.registerHelper('each', function(recipes, options) {
+      var ret = "";
+      for (var i=0; i < recipes.length; i++) {
+        var recipe = recipes[i].recipe;
+        ret = ret + options.fn(recipe);
+      }
+      return ret;
+    });
+    var recipesHTML = recipesTemplate({ plan_type, recipes, date });
+    var navigationHTML = navigationTemplate({ selected: "two_person" });
+    $("#recipes").html(recipesHTML);
+    $("#navigation").html(navigationHTML);
+}
+
+var getOrdinal = function (d) {
+  if(d>3 && d<21) return 'th';
+  switch (d % 10) {
+        case 1:  return "st";
+        case 2:  return "nd";
+        case 3:  return "rd";
+        default: return "th";
+    }
+}
+
+var getDate = function (date) {
+  var months = [ "January", "February", "March", "April", "May", "June",
+               "July", "August", "September", "October", "November", "December" ];
+  var dateObj = new Date(date);
+  var month = months[dateObj.getMonth()];
+  var day = dateObj.getUTCDate();
+  return month + ' ' + day + getOrdinal(day);
+}
+
+var getPlan = function (plan_type, date) {
+  var base = '/api/recipes/';
+  var url = base + plan_type + '/' + date;
+  $.getJSON(url, function (response) {
+      console.log('response', response);
+      var plan = response[plan_type + '_plan'];
+      var recipes = plan.recipes;
+      recipesModel.setState({
+        plan_type: plan.plan.description,
+        date: plan.delivery.date,
+        recipes: recipes
       });
-      var recipesHTML = template({recipes: recipes});
-      $("#container").html(recipesHTML);
-    })
+  });
+}
+
+var init = function () {
+  pickerModel.setState({plan: 'two_person', date: '2016_03_21'});
+  recipesModel.setState({recipes: []});
+  getPlan('two_person','2016_03_21');
 }
 
 document.addEventListener('DOMContentLoaded', init);
